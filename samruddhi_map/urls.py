a51c3b5ae9e7333c7777b359d3e76f4b175017c4
Suyog0056 <162949263+Suyog0056@users.noreply.github.com>
2025-06-21 21:14:07 +0530

    Update urls.py

--- samruddhi_map/urls.py
+++ samruddhi_map/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('maps.urls')),  # make sure 'maps' is the app name
+    path('', include('maps.urls')),  
 ]
